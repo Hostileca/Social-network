13f4616ccb0b043afd6fad05ebbad0d0de9058e1
Kirill Anisko <kirill.anisko13@gmail.com>
2024-08-30 09:09:22 +0300

    simple entities

---
+++ Backend/BlogService/Domain/Entities/Comment.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities;
+
+public class Comment : EntityBase
+{
+    public string Content { get; set; }
+    public Post Post { get; set; }
+    public Guid PostId { get; set; }
+    public Blog Sender { get; set; }
+    public Guid SenderId { get; set; }
+}
