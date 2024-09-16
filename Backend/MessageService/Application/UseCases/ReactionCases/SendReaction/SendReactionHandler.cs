60ca6f08630b2c01e9236d793402188cd95cf9af
Kirill Anisko <kirill.anisko13@gmail.com>
2024-09-16 09:47:10 +0300

    add reactions

---
+++ Backend/MessageService/Application/UseCases/ReactionCases/SendReaction/SendReactionHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.Dtos;
+using Application.Exceptions;
+using Domain.Entities;
+using Domain.Repositories;
+using Mapster;
+using MediatR;
+
+namespace Application.UseCases.ReactionCases.SendReaction;
+
+public class SendReactionHandler(
+    IChatRepository chatRepository,
+    IReactionRepository reactionRepository)
+    : IRequestHandler<SendReactionCommand, ReactionReadDto>
+{
+    public async Task<ReactionReadDto> Handle(SendReactionCommand request, CancellationToken cancellationToken)
+    {
+        var chat = await chatRepository.GetByIdAsync(request.ChatId, cancellationToken);
+
+        if (chat is null)
+        {
+            throw new NotFoundException(typeof(Chat).ToString(), request.ChatId.ToString());
+        }
+
+        var message = chat.Messages.FirstOrDefault(m => m.Id == request.MessageId);
+        
+        if (message is null)
+        {
+            throw new NotFoundException(typeof(Message).ToString(), request.MessageId.ToString());
+        }
+
+        var reaction = request.Adapt<Reaction>();
+        
+        await reactionRepository.AddAsync(reaction, cancellationToken);
+        
+        return reaction.Adapt<ReactionReadDto>();
+    }
+}
