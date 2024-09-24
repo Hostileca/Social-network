897367e9fc7bc0eae08d308c83dd948956449ab7
Kirill Anisko <kirill.anisko13@gmail.com>
2024-09-24 12:26:22 +0300

    angular init

--- Frontend/AngularClient/src/app/app.config.ts
+++ Frontend/AngularClient/src/app/app.config.ts
@@ -2,15 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {CookieService} from "ngx-cookie-service";
-import {provideHttpClient, withInterceptors} from "@angular/common/http";
-import {authInterceptor} from "./Interceptors/auth.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-      provideZoneChangeDetection({ eventCoalescing: true }),
-      provideRouter(routes),
-      provideHttpClient(withInterceptors([authInterceptor])),
-      [CookieService]
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
