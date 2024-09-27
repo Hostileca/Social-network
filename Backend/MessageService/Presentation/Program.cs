bc8b0bbd4c5abbbaeeabab28f4646d38804ccf21
Kirill Anisko <kirill.anisko13@gmail.com>
2024-09-27 11:53:29 +0300

    global fix

--- Backend/MessageService/Presentation/Program.cs
+++ Backend/MessageService/Presentation/Program.cs
@@ -1,14 +1,6 @@
-using System.Text;
 using Application;
-using Hangfire;
 using Infrastructure;
-using Infrastructure.MesssageBroker.Consumers;
-using Infrastructure.SignalR.Hubs;
-using MassTransit;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Presentation;
-using SharedResources.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
