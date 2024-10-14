ff187873402d0b8e5ceeda4d86435ef171323aec
Kirill Anisko <kirill.anisko13@gmail.com>
2024-10-14 10:19:55 +0300

    Get blog chats query fix

--- Backend/MessageService/Application/UseCases/BaseQueries/Paged/PagedQueryValidator.cs
+++ Backend/MessageService/Application/UseCases/BaseQueries/Paged/PagedQueryValidator.cs
@@ -6,7 +6,12 @@ public class PagedQueryValidator : AbstractValidator<PagedQuery>
 {
     public PagedQueryValidator()
     {
-        RuleFor(x => x.PageNumber).Must(x => x > 0);
-        RuleFor(x => x.PageSize).Must(x => x > 0);
+        RuleFor(x => x.PageNumber)
+            .Must(x => x > 0)
+            .WithMessage("Page number must be greater than zero");
+        
+        RuleFor(x => x.PageSize)
+            .Must(x => x > 0)
+            .WithMessage("Page size must be greater than zero");
     }
 }
