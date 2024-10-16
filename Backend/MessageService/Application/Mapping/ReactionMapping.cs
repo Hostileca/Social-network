abef687ab52eb7d91dda6f5120a7be931d5903f4
Kirill Anisko <kirill.anisko13@gmail.com>
2024-10-16 14:22:27 +0300

    add reaction mapping, set sender id requirement to true for reaction

---
+++ Backend/MessageService/Application/Mapping/ReactionMapping.cs
@@ -0,0 +1,24 @@
+﻿using Application.UseCases.ReactionCases.Commands.SendReaction;
+using Domain.Entities;
+using Mapster;
+using SharedResources.Dtos;
+
+namespace Application.Mapping;
+
+public class ReactionMapping : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<Reaction, ReactionReadDto>()
+            .Map(x => x.Id, x => x.Id)
+            .Map(x => x.Emoji, x => x.Emoji)
+            .Map(x => x.SenderId, x => x.SenderId)
+            .Map(x => x.MessageId, x => x.MessageId);
+        
+        config.NewConfig<SendReactionCommand, Reaction>()
+            .Map(x => x.Id, x => Guid.NewGuid())
+            .Map(x => x.Emoji, x => x.Emoji)
+            .Map(x => x.SenderId, x => x.UserBlogId)
+            .Map(x => x.MessageId, x => x.MessageId);
+    }
+}
